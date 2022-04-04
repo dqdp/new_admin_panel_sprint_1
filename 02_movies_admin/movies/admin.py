73847bd1cb53872ac6976ddc536cfb03dd4cb984
Alexey Smirnov <ziiggy13@gmail.com>
2022-04-04 11:40:39 +0300

    added autocomplete

--- 02_movies_admin/movies/admin.py
+++ 02_movies_admin/movies/admin.py
@@ -7,26 +7,27 @@ from .models import Genre, Filmwork, GenreFilmwork, Person, PersonFilmWork
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
-     # Отображение полей в списке
+    # Отображение полей в списке
     list_display = ('name', 'description',)
     # Поиск по полям
-    search_fields = ('name', 'description', 'id',) 
+    search_fields = ('name', 'description', 'id',)
 
 
 class PersonFilmworkInline(admin.TabularInline):
     model = PersonFilmWork
+    autocomplete_fields = ('person',)
 
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
-    #inlines = (PersonFilmworkInline,)
 
     list_display = ('full_name',)
-    search_fields = ('full_name','id',) 
+    search_fields = ('full_name',)
 
 
 class GenreFilmworkInline(admin.TabularInline):
     model = GenreFilmwork
+
 
 @admin.register(Filmwork)
 class FilmworkAdmin(admin.ModelAdmin):
@@ -35,6 +36,6 @@ class FilmworkAdmin(admin.ModelAdmin):
     # Отображение полей в списке
     list_display = ('title', 'type', 'creation_date', 'rating',)
     # Фильтрация в списке
-    list_filter = ('type','rating',)
+    list_filter = ('type', 'rating',)
     # Поиск по полям
-    search_fields = ('title', 'description', 'id',) 
+    search_fields = ('title', 'description', 'id',)
